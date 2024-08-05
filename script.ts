705d0a6857fdb367eef7df7f8915ba7e124e0255
eye-co <aikolauren@gmail.com>
2024-08-04 21:09:42 -0400

    added point interface and random point generator

--- script.ts
+++ script.ts
@@ -7,3 +7,16 @@ box.height = 500;
 ctx.moveTo(0, 0);
 ctx.lineTo(200, 100);
 ctx.stroke();
+
+interface Point {
+    x: number;
+    y: number;
+}
+
+const myPoints: Point[] = [];
+
+function randomPointGenerator(ct: number): void {
+    for (let i = 0; i < ct; i++) {
+        myPoints.push({x: Math.random() * box.width, y : Math.random() * box.height})
+    }
+}
