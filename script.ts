ab9faa03a2039d659b9e778bca4f21b56c631687
eye-co <aikolauren@gmail.com>
2024-08-04 20:52:33 -0400

    added canvas styling and changed colors

--- script.ts
+++ script.ts
@@ -1,1 +1,9 @@
 // script code here
+const box = document.getElementById("clusterCanvas");
+const ctx = box.getContext("2d");
+box.width = 500;
+box.height = 500;
+
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
