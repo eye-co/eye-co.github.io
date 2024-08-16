95836bbfa3bdc7539199b45dad28e9726a7196a8
eye-co <aikolauren@gmail.com>
2024-08-16 18:22:44 -0400

    Added code to try and make random dots be drawn for clustering

--- script.ts
+++ script.ts
@@ -1,4 +1,5 @@
 // script code here
+const aiko: number = 5;
 const box = document.getElementById("clusterCanvas");
 const ctx = box.getContext("2d");
 box.width = 500;
@@ -20,3 +21,15 @@ function randomPointGenerator(ct: number): void {
         myPoints.push({x: Math.random() * box.width, y : Math.random() * box.height})
     }
 }
+
+function drawPoints(points: Point[]): void {
+    myPoints.forEach(point => {
+        ctx?.beginPath();
+        ctx?.arc(point.x, point.y, 50, 0, Math.PI * 2); // x, y, radius, start angle, end angle
+        ctx!.fillStyle = 'black';
+        ctx?.fill();
+    });
+}
+
+randomPointGenerator(100)
+drawPoints(myPoints)
