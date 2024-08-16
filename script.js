4b5250cb85e66f98d15122a6e2c25b9d397ed6c3
eye-co <aikolauren@gmail.com>
2024-08-16 18:29:15 -0400

    converted script from ts to js, then deleted tsconfig since we are using ts

---
+++ script.js
@@ -0,0 +1,33 @@
+// script code here
+const aiko = 5
+const box = document.getElementById("clusterCanvas")
+const ctx = box.getContext("2d")
+box.width = 500
+box.height = 500
+
+ctx.moveTo(0, 0)
+ctx.lineTo(200, 100)
+ctx.stroke()
+
+const myPoints = []
+
+function randomPointGenerator(ct) {
+  for (let i = 0; i < ct; i++) {
+    myPoints.push({
+      x: Math.random() * box.width,
+      y: Math.random() * box.height
+    })
+  }
+}
+
+function drawPoints(points) {
+  myPoints.forEach(point => {
+    ctx?.beginPath()
+    ctx?.arc(point.x, point.y, 50, 0, Math.PI * 2) // x, y, radius, start angle, end angle
+    ctx.fillStyle = "black"
+    ctx?.fill()
+  })
+}
+
+randomPointGenerator(100)
+drawPoints(myPoints)
--- script.ts
+++ script.ts
@@ -1,35 +1,35 @@
-// script code here
-const aiko: number = 5;
-const box = document.getElementById("clusterCanvas");
-const ctx = box.getContext("2d");
-box.width = 500;
-box.height = 500;
+// // script code here
+// const aiko: number = 5;
+// const box = document.getElementById("clusterCanvas");
+// const ctx = box.getContext("2d");
+// box.width = 500;
+// box.height = 500;
 
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
+// ctx.moveTo(0, 0);
+// ctx.lineTo(200, 100);
+// ctx.stroke();
 
-interface Point {
-    x: number;
-    y: number;
-}
+// interface Point {
+//     x: number;
+//     y: number;
+// }
 
-const myPoints: Point[] = [];
+// const myPoints: Point[] = [];
 
-function randomPointGenerator(ct: number): void {
-    for (let i = 0; i < ct; i++) {
-        myPoints.push({x: Math.random() * box.width, y : Math.random() * box.height})
-    }
-}
+// function randomPointGenerator(ct: number): void {
+//     for (let i = 0; i < ct; i++) {
+//         myPoints.push({x: Math.random() * box.width, y : Math.random() * box.height})
+//     }
+// }
 
-function drawPoints(points: Point[]): void {
-    myPoints.forEach(point => {
-        ctx?.beginPath();
-        ctx?.arc(point.x, point.y, 50, 0, Math.PI * 2); // x, y, radius, start angle, end angle
-        ctx!.fillStyle = 'black';
-        ctx?.fill();
-    });
-}
+// function drawPoints(points: Point[]): void {
+//     myPoints.forEach(point => {
+//         ctx?.beginPath();
+//         ctx?.arc(point.x, point.y, 50, 0, Math.PI * 2); // x, y, radius, start angle, end angle
+//         ctx!.fillStyle = 'black';
+//         ctx?.fill();
+//     });
+// }
 
-randomPointGenerator(100)
-drawPoints(myPoints)
+// randomPointGenerator(100)
+// drawPoints(myPoints)
